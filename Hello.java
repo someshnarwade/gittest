915ab1bf4ff83c20ca69c3823764d5a6b9ce8e01
SOMESH NARWADE <someshnarwade@gmail.com>
2020-01-27 11:35:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ class Hello
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Welcome to Java Program from Github"); 
     } 
 }
